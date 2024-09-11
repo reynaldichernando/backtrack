a83e0ea52b81bc424a5496bd67f80946e7a6ced2
Reynaldi Chernando <reynaldichernando@gmail.com>
2024-09-11 18:37:36 +0700

    Init PWA

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,44 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
+const APP_NAME = "BackTrack";
+const APP_DEFAULT_TITLE = "BackTrack";
+const APP_TITLE_TEMPLATE = "BackTrack";
+const APP_DESCRIPTION = "Standalone YouTube Media Library";
+
 export const metadata: Metadata = {
-  title: "BackTrack",
-  description: "Standalone YouTube Media Library",
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
 };
 
 export default function RootLayout({
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+import withSerwistInit from "@serwist/next";
+
+const withSerwist = withSerwistInit({
+  // Note: This is only an example. If you use Pages Router,
+  // use something else that works, such as "service-worker/index.ts".
+  swSrc: "app/sw.ts",
+  swDest: "public/sw.js",
+});
+
 const nextConfig = {
   images: {
     remotePatterns: [
@@ -12,4 +22,4 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+export default withSerwist(nextConfig);
