d36b4dca703d87d7be2cab23933f432f90919505
Reynaldi Chernando <reynaldichernando@gmail.com>
2024-09-17 02:20:04 +0700

    Enable offline cache

--- app/sw.ts
+++ app/sw.ts
@@ -1,11 +1,7 @@
 import { defaultCache } from "@serwist/next/worker";
 import type { PrecacheEntry, SerwistGlobalConfig } from "serwist";
-import { Serwist } from "serwist";
+import { ExpirationPlugin, NetworkFirst, Serwist } from "serwist";
 
-// This declares the value of `injectionPoint` to TypeScript.
-// `injectionPoint` is the string that will be replaced by the
-// actual precache manifest. By default, this string is set to
-// `"self.__SW_MANIFEST"`.
 declare global {
   interface WorkerGlobalScope extends SerwistGlobalConfig {
     __SW_MANIFEST: (PrecacheEntry | string)[] | undefined;
@@ -19,7 +15,22 @@ const serwist = new Serwist({
   skipWaiting: true,
   clientsClaim: true,
   navigationPreload: true,
-  runtimeCaching: defaultCache,
+  runtimeCaching: [
+    ...defaultCache,
+    {
+      matcher: /\/.*/i,
+      handler: new NetworkFirst({
+        cacheName: 'dynamicCache',
+        plugins: [
+          new ExpirationPlugin({
+            maxEntries: 200,
+            maxAgeSeconds: 7 * 24 * 60 * 60, // 7 days
+            maxAgeFrom: "last-used",
+          }),
+        ],
+      }),
+    },
+  ],
 });
 
 serwist.addEventListeners();
