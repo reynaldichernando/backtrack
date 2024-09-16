d3345575af8c7cc7ae4dd0a76d12a296d6b4a1cf
Reynaldi Chernando <reynaldichernando@gmail.com>
2024-09-16 16:18:33 +0700

    Disable scroll when on player page

--- components/Main.tsx
+++ components/Main.tsx
@@ -176,49 +176,47 @@ export default function Main() {
 
   return (
     <>
-      <div className="flex flex-col">
-        <div className={`md:flex ${currentView == "detail" ? "overflow-hidden" : ""}`}>
-          <div className="md:w-1/4 p-4 space-y-4 md:overflow-auto md:border-r">
-            <div className="flex items-center space-x-2">
-              <img src="./144.png" alt="BackTrack Logo" className="w-8 h-8 rounded" />
-              <h1 className="text-2xl font-bold">BackTrack</h1>
-            </div>
-            <AddVideoDialog onAddVideo={handleAddVideo} />
+      <div className={`md:flex ${currentView == "detail" ? "overflow-hidden h-screen" : ""}`}>
+        <div className="md:w-1/4 p-4 space-y-4 md:overflow-auto md:border-r">
+          <div className="flex items-center space-x-2">
+            <img src="./144.png" alt="BackTrack Logo" className="w-8 h-8 rounded" />
+            <h1 className="text-2xl font-bold">BackTrack</h1>
           </div>
-          <div className="md:w-3/4 p-4 md:overflow-auto pb-28 md:h-screen">
-            <MyVideos videos={videos} onSelectVideo={handleSelectVideo} onDeleteVideo={handleDeleteVideo} />
-            <Player
-              currentVideo={currentVideo}
-              currentView={currentView}
-              isPlaying={isPlaying}
-              onBack={handlePlayerBack}
-              onTogglePlay={handleTogglePlay}
-              onClickPrevTrack={prevTrack}
-              onClickNextTrack={nextTrack}
-              updateMediaSources={updateMediaSources}
+          <AddVideoDialog onAddVideo={handleAddVideo} />
+        </div>
+        <div className="md:w-3/4 p-4 md:overflow-auto pb-28 md:h-screen">
+          <MyVideos videos={videos} onSelectVideo={handleSelectVideo} onDeleteVideo={handleDeleteVideo} />
+          <Player
+            currentVideo={currentVideo}
+            currentView={currentView}
+            isPlaying={isPlaying}
+            onBack={handlePlayerBack}
+            onTogglePlay={handleTogglePlay}
+            onClickPrevTrack={prevTrack}
+            onClickNextTrack={nextTrack}
+            updateMediaSources={updateMediaSources}
+          >
+            <video
+              className="aspect-video w-full bg-gray-200 rounded-lg mb-4 flex items-center justify-center"
+              playsInline
+              autoPlay
+              poster={currentVideo?.thumbnail}
+              src={videoSrc}
+              ref={videoRef}
             >
-              <video
-                className="aspect-video w-full bg-gray-200 rounded-lg mb-4 flex items-center justify-center"
-                playsInline
-                autoPlay
-                poster={currentVideo?.thumbnail}
-                src={videoSrc}
-                ref={videoRef}
-              >
-                <source src={videoSrc} type="video/webm" />
-              </video>
-              <audio
-                className="aspect-video w-full bg-gray-200 rounded-lg mb-4 flex items-center justify-center"
-                autoPlay
-                src={audioSrc}
-                ref={audioRef}
-                onPlay={setHandlers}
-                onEnded={nextTrack}
-              >
-                <source src={audioSrc} type="audio/webm" />
-              </audio>
-            </Player>
-          </div>
+              <source src={videoSrc} type="video/webm" />
+            </video>
+            <audio
+              className="aspect-video w-full bg-gray-200 rounded-lg mb-4 flex items-center justify-center"
+              autoPlay
+              src={audioSrc}
+              ref={audioRef}
+              onPlay={setHandlers}
+              onEnded={nextTrack}
+            >
+              <source src={audioSrc} type="audio/webm" />
+            </audio>
+          </Player>
         </div>
       </div>
       <MiniPlayer currentVideo={currentVideo} currentView={currentView} isPlaying={isPlaying} onClick={handleMiniPlayerClick} onTogglePlay={handleTogglePlay} />
