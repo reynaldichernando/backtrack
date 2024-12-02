6038d3ed76ff09848d5946d92017699cae264e2e
Reynaldi Chernando <reynaldichernando@gmail.com>
2024-12-03 01:45:29 +0700

    Revert "Attempt ios pwa"
    This reverts commit 6ab807e0ec1a4fa825c0bed110f1e3418115dd83.

--- app/sw.ts
+++ app/sw.ts
@@ -1,5 +1,6 @@
+import { defaultCache } from "@serwist/next/worker";
 import type { PrecacheEntry, SerwistGlobalConfig } from "serwist";
-import { Serwist, CacheFirst } from "serwist";
+import { Serwist } from "serwist";
 
 declare global {
   interface WorkerGlobalScope extends SerwistGlobalConfig {
@@ -13,16 +14,8 @@ const serwist = new Serwist({
   precacheEntries: self.__SW_MANIFEST,
   skipWaiting: true,
   clientsClaim: true,
-  navigationPreload: false,
-  runtimeCaching: [
-    {
-      // Cache everything for offline access
-      matcher: () => true,
-      handler: new CacheFirst({
-        cacheName: "app-cache"
-      }),
-    },
-  ],
+  navigationPreload: true,
+  runtimeCaching: defaultCache,
 });
 
 serwist.addEventListeners();
